b5a6cbc5a63632dfb9738ad350d1b0008df394f5
vincent <vincentliangws@gmail.com>
2014-08-20 09:28:10 +0800

    demo

---
+++ mygo/src/form/form.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
+
+	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello vincent!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+
+	err := http.ListenAndServe(":9090", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
---
+++ mygo/src/interfaceDemo/bufferedChannels.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int, 2)
+
+	c <- 2
+	c <- 1
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
